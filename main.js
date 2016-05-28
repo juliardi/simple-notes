c6a0669baf887e27873bc378d2554e289b8c3cf7
Muhammad Safri Juliardi <juliardi@student.uns.ac.id>
2016-05-28 21:59:26 +0700

    edit app structure

--- main.js
+++ main.js
@@ -1,57 +1,28 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
+const electron = require('electron');
+const app = electron.app;
 
 const {Menu, Tray} = require('electron');
-
-const Notes = require('./lib/notes.js');
+const traymenu = require('./lib/core/traymenu.js');
 
 let appIcon = null;
 
 function createTray() {
-  appIcon = new Tray("img/icon.png");
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'New Notes', click() {
-        var notes = new Notes();
-      }
-    },
-    {
-      type: 'separator',
-    },
-    {
-      label: 'Exit', click() {
-        app.quit();
-      }
-    }
-  ]);
-  appIcon.setToolTip("Simple Notes Application");
-  appIcon.setContextMenu(contextMenu);
+    appIcon = new Tray("img/icon.png");
+    appIcon.setToolTip("Simple Notes Application");
+    const contextMenu = Menu.buildFromTemplate(traymenu(app));
+    appIcon.setContextMenu(contextMenu);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-
-// app.on('ready', createWindow)
 app.on('ready', createTray);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+});
